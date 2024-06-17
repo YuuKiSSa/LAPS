dae647d045f1479f8e3dd30bb95f60184a62c5e8
Fanyzt <e1280877@u.nus.edu>
2024-06-17 16:26:10 +0800

    css&session

--- src/main/java/sg/edu/nus/spring_laps/controller/LoginController.java
+++ src/main/java/sg/edu/nus/spring_laps/controller/LoginController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import sg.edu.nus.spring_laps.model.Admin;
 import sg.edu.nus.spring_laps.model.Staff;
 import sg.edu.nus.spring_laps.service.StaffService;
@@ -23,18 +26,21 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam("userId") String userId, @RequestParam("password") String password, @RequestParam("role") String role, Model model) {
-        if (role.equals("Admin")) {
+    public String login(HttpServletRequest request,@RequestParam("userId") String userId, @RequestParam("password") String password, @RequestParam("role") String role, Model model) {
+    	HttpSession session = request.getSession();
+    	if (role.equals("Admin")) {
             Admin admin = staffService.authenticateAdmin(userId, password);
             if (admin != null) {
                 model.addAttribute("user", admin);
+                session.setAttribute("userId",admin.getUserId());
                 return "adminHome";
             }
         } else {
             Staff staff = staffService.authenticateStaff(userId, password);
             if (staff != null) {
                 model.addAttribute("user", staff);
-                if (staff.getHierarchy() == 1) {
+                session.setAttribute("userId",staff.getUserId());
+                if (staff.getHierarchy() >= 1) {
                     return "managerHome"; 
                 } else {
                     return "employeeHome"; 
