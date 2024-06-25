f103ab3c8016a813c4daf283617e87a2551eea47
YuuKiSSa <e1241986@u.nus.edu>
2024-06-25 09:25:56 +0800

    modify css of staff dashboard, delete bin file

--- bin/src/main/resources/static/JavaScript/search-result-script.js
+++
@@ -1,8 +0,0 @@
-function enableEdit(button) {
-    var inputFields = button.parentNode.parentNode.querySelectorAll('input');
-    inputFields.forEach(function(input) {
-        if (input.name !== 'userId') { // Ensure userId remains readonly
-            input.removeAttribute('readonly');
-        }
-    });
-}
